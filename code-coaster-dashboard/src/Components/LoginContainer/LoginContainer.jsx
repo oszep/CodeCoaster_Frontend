b31da239b1020b3754db4bbe1369bb1726f97463
Dulce Bucio <dulcenahomibucio@gmail.com>
2024-03-21 15:28:57 -0600

    login container advancement

--- code-coaster-dashboard/src/Components/LoginContainer/LoginContainer.jsx
+++ code-coaster-dashboard/src/Components/LoginContainer/LoginContainer.jsx
@@ -3,8 +3,15 @@ import "./LoginContainer.css"
 function LoginContainer() {
     return(
         <>
-            <div className="loginContainer">
+            <div className="login-container">
                 <div className="header">
+                    <img className="aulify-games-logo" src="src/UI/aulify-games-logo.png"/>
+                </div>
+                <div className="logo-container">
+                    <img className="login-image" src="src/UI/code-coaster-logo.png"/>
+                </div>
+                <div className="button-container">
+                    <button className="loginButton">Iniciar sesión</button>
                     
                 </div>
             </div>
