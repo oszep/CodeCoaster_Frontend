d984a4a9ccbd3052370454f928d1e620cdfad0ff
DesireeEspinosa <desireeespinosa245@gmail.com>
2024-03-22 04:30:59 -0600

    LoginForm without functionality

--- code-coaster-dashboard/src/Components/LoginContainer/LoginContainer.jsx
+++ code-coaster-dashboard/src/Components/LoginContainer/LoginContainer.jsx
@@ -1,22 +1,36 @@
-import "./LoginContainer.css"
+import "./LoginContainer.css";
+import React, { useState } from "react";
 
 function LoginContainer() {
-    return(
+    const [showLoginForm, setShowLoginForm] = useState(false);
+
+    const handleLoginClick = () => {
+        setShowLoginForm(true);
+    };
+
+    return (
         <>
             <div className="login-container">
                 <div className="header">
-                    <img className="aulify-games-logo" src="src/UI/aulify-games-logo.png"/>
+                    <img className="aulify-games-logo" src="src/UI/aulify-games-logo.png" alt="Aulify Games Logo" />
                 </div>
                 <div className="logo-container">
-                    <img className="login-image" src="src/UI/code-coaster-logo.png"/>
+                    <img className="login-image" src="src/UI/code-coaster-logo.png" alt="Code Coaster Logo" />
                 </div>
                 <div className="button-container">
-                    <button className="loginButton">Iniciar sesión</button>
-                    
+                    {!showLoginForm ? (
+                        <button className="loginButton" onClick={handleLoginClick}>Iniciar sesión</button>
+                    ) : (
+                        <form className="loginForm">
+                            <input type="text" placeholder="Email" required />
+                            <input type="password" placeholder="Contraseña" required />
+                            <button type="submit">Continuar</button>
+                        </form>
+                    )}
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default LoginContainer
+export default LoginContainer;
